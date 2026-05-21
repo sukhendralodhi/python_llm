f5d8cef7c9aad3f4a248e758ddab120c96930745
Sukhendra <developer.sukhendra@gmail.com>
2026-05-21 21:36:26 +0530

    opp intro

---
+++ python100/oops_python.py
@@ -0,0 +1,11 @@
+# object oriented programming (OOP) is a programming paradigm that uses objects and classes to structure code. It allows for the creation of reusable and modular code, making it easier to manage and maintain.
+
+# What is an object?
+# An object is an instance of a class. It is a self-contained unit that contains data and behavior. Objects can represent real-world entities, such as a person, a car, or a bank account.
+
+# what is a class?
+# A class is a blueprint for creating objects. It defines the properties and behaviors that the objects created from the class will have. A class can be thought of as a template for creating objects.
+
+# Generality to Specificity
+# In OOP, we can create a general class that defines common properties and behaviors, and then create specific classes that inherit from the general class. This allows us to reuse code and avoid duplication.
+

ee309d81d02bf09a9fdf66f634fcc4415cb22823
Sukhendra <developer.sukhendra@gmail.com>
2026-05-22 00:14:12 +0530

    megic method

---
+++ python100/megic_method.py
@@ -0,0 +1,9 @@
+# Magic Method in Python
+
+# Magic methods in Python are special methods that have double underscores at the beginning and end of their names. They are also known as dunder methods (short for "double underscore"). These methods allow us to define the behavior of our objects in certain situations, such as when we want to perform arithmetic operations, compare objects, or represent our objects as strings.
+
+# Note: We can not call constructor method directly, it is called automatically when we create an object of the class. However, we can call other methods directly on the object.
+
+# Constructors are special methods that are called when an object is created. They are used to initialize the attributes of the object. The constructor method in Python is defined using the __init__ method.
+
+# Use of constructor method in Python is to initialize the attributes of the object when it is created. For example, if we have a class called Person, we can use the constructor method to initialize the name and age attributes of the Person object when it is created.

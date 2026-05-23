a35321309d476e330e5f02d3362c1e6a3325fd51
Sukhendra <developer.sukhendra@gmail.com>
2026-05-23 23:50:59 +0530

    aggregation in class

--- python100/exmaple_aggregation.py
+++ python100/exmaple_aggregation.py
@@ -1,3 +1,73 @@
+
+# 🔹 Aggregation in Python OOP
+# Aggregation is a relationship where one class contains another class object.
+
+# It represents a "has-a" relationship.
+
+# Example:
+# A Department has Teacher
+# A Car has Engine
+# A School has Students
+
+# The contained object can exist independently.
+
+
+class Engine:
+    def start(self):
+        print("Engine start")
+
+
+class Car:
+    
+    def __init__(self, engine):
+        self.engine = engine # Aggregation 
+
+    def drive(self):
+        self.engine.start()
+        print("Car is running")
+
+
+
+# eng = Engine()     # Engine object created separately
+# car = Car(eng)     # Pass engine object to Car
+# car.drive()
+
+# 🔹 Why This is Aggregation?
+    # Because:
+    # Engine can exist without Car
+    # Car uses Engine
+    # Objects are loosely connected
+
+
+# 🔹 Real-World Example
+
+class Student:
+
+    def __init__(self, name):
+        self.name = name
+
+
+class College:
+
+    def __init__(self, student):
+        self.student = student
+
+
+    def show(self):
+        print("Student name", self.student.name)
+
+
+s1 = Student("Sukhendra")
+c1 = College(s1)
+
+c1.show()
+        
+
+
+
+
+
+
 class Customer:
 
     def __init__(self, name, gender, address):
@@ -24,12 +94,12 @@ class Address:
         self.state = new_state
 
 
-add = Address("Indore", 452001, "Madhya Pradesh")
-customer = Customer("Sanju", "Male", add)
+# add = Address("Indore", 452001, "Madhya Pradesh")
+# customer = Customer("Sanju", "Male", add)
 
-customer.edit_profile("Sardar", "Jhanshi", 559900, "Uttar Pradesh")
+# customer.edit_profile("Sardar", "Jhanshi", 559900, "Uttar Pradesh")
 
-print(customer.address.city)
-print(customer.address.state)
-print(customer.address.pincode)
+# print(customer.address.city)
+# print(customer.address.state)
+# print(customer.address.pincode)
         
---
+++ python100/inheritance.py
@@ -0,0 +1,66 @@
+# Inheritance = Inheritance is a core concept of object oriented programming that allow one class (child/derived class) to reuse the properties and method of another class (parent/base class)
+
+# What is inheritance?
+# Inheritance let's you create a new class based on an existing class.
+# The new class inherit attributes (variables) and behaviur (methods) from the parent class.
+
+# Think of like it 
+
+# Parent class => General Concept 
+# Child class => More specific version 
+
+# Basic Syntax In Python
+
+class Parent:
+
+    def display(self):
+        print("This is a parent class")
+
+
+class Child(Parent):
+
+    def show(self):
+        print("This is a child class")
+
+
+# obj = Child()
+# obj.display()
+# obj.show()
+
+# Note: Private member are not inherited 
+
+
+class User:
+
+    def login(self):
+        print("Login")
+
+    def register(self):
+        print("Register")
+
+
+class Student(User):
+
+    def enroll(self):
+        print("Enroll")
+
+    def review(self):
+        print("Review")
+
+
+# child can access parent class methods and attributes 
+
+# student1 = Student()
+# student1.login()
+# student1.register()
+# student1.enroll()
+# student1.review()
+
+
+# But parent can not access child class methods and attributes 
+
+# user = User()
+# user.login()
+# user.register()
+# user.enroll()
+# user.review()

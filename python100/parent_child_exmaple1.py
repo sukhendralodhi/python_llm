7e830c447ba8c6a81d995e913509159685be1bcb
Sukhendra <developer.sukhendra@gmail.com>
2026-05-24 00:29:53 +0530

    Agar child ke pas apna koi constructor ni hai to parent ka constructor call ho jata hai but agar child ke pass apne constructor hai to parent ka call ni hota hai

---
+++ python100/parent_child_exmaple1.py
@@ -0,0 +1,23 @@
+class Parent:
+
+    def __init__(self, num):
+        self.__num = num
+
+    def get_num(self):
+
+        return self.__num
+    
+
+class Child(Parent):
+
+    def show(self):
+
+        print("This is an child class")
+
+
+son = Child(100)
+print(son.get_num())
+son.show()
+
+    
+        
---
+++ python100/parent_child_exmaple2.py
@@ -0,0 +1,26 @@
+class Parent:
+
+    def __init__(self, num):
+        self.__num = num
+
+    def get_num(self):
+        return self.__num
+    
+
+
+class Child(Parent):
+
+    def __init__(self, val, num):
+        self.__val = val
+
+
+    def get_val(self):
+        return self.__val
+    
+
+son = Child(100, 10)
+print("Parent num: ", son.get_num())
+print("Child val: ", son.get_val())
+
+
+# Note: Agar child ke pas apna koi constructor ni hai to parent ka constructor call ho jata hai but agar child ke pass apne constructor hai to parent ka call ni hota hai 
